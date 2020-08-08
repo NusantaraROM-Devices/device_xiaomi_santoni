7b7f9241e3effc7e002924f5a616ce172bc988eb
zhantech <hifzhan41@gmail.com>
2020-08-08 00:46:24 +0000

    santoni: fix thermal

--- device.mk
+++ device.mk
@@ -462,6 +462,7 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-service \
     thermal.msm8937
 
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
 # USB HAL
